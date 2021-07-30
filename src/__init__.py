5128362cf52d8cdf8c87f70545d2a62c36809893
Peichong <fupeichong@gmail.com>
2021-07-30 20:32:23 +0800

    use config files

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,16 @@
+import os
 from flask import Flask
 
-def create_app(config_filename=None):
+def create_app(env = os.environ['FLASK_ENV']):
     app = Flask(__name__)
+    app.config.from_pyfile(get_config_filename(app, env))
     register_blueprints(app)
     return app
+
+
+def get_config_filename(app, env):
+    root = os.path.dirname(app.root_path)
+    return f'{root}/config/{env}.cfg'
 
 
 def register_blueprints(app):
