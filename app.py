050ebfd1555112fcbfe8a28797eeb5a734c09d41
Peichong <fupeichong@gmail.com>
2021-08-08 00:30:08 +0800

    add cli command to start worker for task queue

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+import redis
+from rq import Connection, Worker
 from src import create_app
 
 app = create_app()
+
+
+@app.cli.command('start_worker')
+def start_worker():
+    with Connection(app.redis):
+        worker = Worker(app.task_queue.name)
+        worker.work()
