c9d050694de9bc91542c82df9c8f6ffbc0396e13
Peichong <fupeichong@gmail.com>
2021-08-13 18:30:03 +0900

    add example to serialize object and store in redis

--- src/ml/routes.py
+++ src/ml/routes.py
@@ -1,19 +1,25 @@
 from flask import current_app, request, jsonify
 from . import ml_blueprint
 from src.lib.tasks import ExampleTask
+import pickle
 
 
 @ml_blueprint.route('/ml/train', methods=['POST'])
 def train():
     # placeholder logic
     task = ExampleTask()
-    delay = int(request.args.get('delay', 1))
-    job = current_app.task_queue.enqueue(task.run, delay)
-    return jsonify({ 'status': 'ok', 'message': f'Job #{job.get_id()} queued' })
+    task.message = request.args.get('message', 'sample text')
+
+    serialized = pickle.dumps(task)
+    current_app.redis.set('pickled_obj', serialized)
+    deserialized = pickle.loads(current_app.redis.get('pickled_obj'))
+    return jsonify({ 'status': 'ok', 'message': deserialized.message })
 
 
 @ml_blueprint.route('/ml/predict', methods=['GET'])
 def predict():
     # placeholder logic
-
-    return jsonify({ 'status': 'ok' })
+    task = ExampleTask()
+    delay = int(request.args.get('delay', 1))
+    job = current_app.task_queue.enqueue(task.run, delay)
+    return jsonify({ 'status': 'ok', 'message': f'Job #{job.get_id()} queued' })
