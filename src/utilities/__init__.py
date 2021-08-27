9ccbf499589b44ef23f4063161188a14331622a7
Peichong <fupeichong@gmail.com>
2021-08-27 18:40:04 +0800

    add general error handling for app

--- src/utilities/__init__.py
+++ src/utilities/__init__.py
@@ -1,5 +1,7 @@
 from flask import make_response, jsonify
 from flask import Blueprint
+from werkzeug.exceptions import HTTPException
+
 
 utilities_blueprint = Blueprint('utilities', __name__)
 
@@ -7,7 +9,16 @@ from . import routes
 
 
 def add_error_handling(app):
+    @app.errorhandler(Exception)
+    def handle_error(e):
+        if isinstance(e, HTTPException):
+            return e
+
+        content = { 'status': 'error', 'code': 500, 'message': str(e) }
+        return make_response(jsonify(content), 500)
+
+
     @app.errorhandler(404)
     def page_not_found(e):
-        content = { 'status': 'error', 'code': e.code, 'msg': e.description }
+        content = { 'status': 'error', 'code': e.code, 'message': e.description }
         return make_response(jsonify(content), 404)
