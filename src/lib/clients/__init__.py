c847f9d5a355798c99f55911c0547d746c053c24
Peichong <fupeichong@gmail.com>
2021-08-29 11:33:23 +0800

    add frontend client

--- src/lib/clients/__init__.py
+++ src/lib/clients/__init__.py
@@ -1,2 +1,3 @@
 from .iex_client import Iex
 from .finnhub_client import Finnhub
+from .frontend_client import FrontendClient
---
+++ src/lib/clients/frontend_client.py
@@ -0,0 +1,45 @@
+import os, requests
+from flask import current_app
+from config import get_config
+
+
+def handle_http_errors(fn):
+    def wrapper(endpoint, **kwargs):
+        try:
+            return fn(endpoint, **kwargs)
+        except requests.exceptions.HTTPError as err:
+            return {
+                'status': 'error',
+                'code': res.status_code,
+                'raw_response': err.response
+            }
+
+    return wrapper
+
+
+class FrontendClient:
+    @staticmethod
+    @handle_http_errors
+    def get(endpoint, **kwargs):
+        url = FrontendClient.build_url(endpoint)
+        res = requests.get(url, **kwargs)
+        res.raise_for_status()
+        return { 'status': 'ok', 'data': res.json() }
+
+
+    @staticmethod
+    def get_price_histories(stock_id, start=None, end=None):
+        endpoint = f'/stocks/{stock_id}/price_histories'
+        params = { 'date_start': start, 'date_end': end }
+        return FrontendClient.get(endpoint, params=params)
+
+
+    @staticmethod
+    def build_url(endpoint):
+        c_app = current_app
+        if not c_app:
+            from app import app as c_app
+
+        if endpoint[0] != '/':
+            endpoint = '/' + endpoint
+        return f'{c_app.frontend_url}{endpoint}'
