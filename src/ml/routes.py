601c0fc1f7762520dfb4121418877f2a88d33aad
Peichong <fupeichong@gmail.com>
2021-08-29 11:35:34 +0800

    minor cleanup of model training route

--- src/ml/routes.py
+++ src/ml/routes.py
@@ -2,7 +2,6 @@ from flask import current_app, request, jsonify
 from . import ml_blueprint
 from src.lib.parameter_checking import require_params
 from src.lib.jobs import ModelTrainingJob
-import pickle
 
 
 @ml_blueprint.route('/ml/model_training', methods=['POST'])
@@ -10,9 +9,9 @@ import pickle
 def train(config_id, stocks, data_range):
     results = []
     for stock_id in stocks:
-        res = send_training_job(config_id, stock_id, data_range)
+        res = enqueue_training_job(config_id, stock_id, data_range)
         results.append(res)
-    return jsonify({ 'status': 'ok', 'message': 'Jobs request processed', 'results': results })
+    return jsonify({ 'status': 'ok', 'message': 'Job requests processed', 'results': results })
 
 
 @ml_blueprint.route('/ml/predict', methods=['GET'])
@@ -24,12 +23,13 @@ def predict():
     return jsonify({ 'status': 'ok', 'message': f'Job #{job.get_id()} queued' })
 
 
-def send_training_job(config_id, stock_id, data_range):
+def enqueue_training_job(config_id, stock_id, data_range):
+    jid = f'modal_config_{config_id}_stock_{stock_id}'
     result = { 'config_id': config_id, 'stock_id': stock_id }
 
     try:
         training = ModelTrainingJob(config_id, stock_id, *data_range)
-        job = current_app.training_queue.enqueue(training.run)
+        job = current_app.training_queue.enqueue(training.run, job_id=jid, job_timeout=1200)
         result.update({ 'status': 'ok', 'job_id': job.get_id() })
     except Exception as e:
         result.update({ 'status': 'error', 'error_message': str(e) })
