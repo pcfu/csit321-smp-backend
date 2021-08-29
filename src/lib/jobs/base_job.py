b7af938561dea1ab3b2f5d26e38068eb977c6634
Peichong <fupeichong@gmail.com>
2021-08-29 11:33:42 +0800

    update BaseJob with some helpers

--- src/lib/jobs/base_job.py
+++ src/lib/jobs/base_job.py
@@ -1,8 +1,17 @@
+import pickle
+from flask import current_app
 from abc import ABC, abstractmethod
+from src.lib.clients import FrontendClient
 from .validation_mixin import ValidationMixin
 
 
 class BaseJob(ABC, ValidationMixin):
+    @abstractmethod
+    def __init__(self):
+        super().__init__()
+        self.frontend = FrontendClient
+
+
     @abstractmethod
     def run(self, *args, **kwargs):
         pass
@@ -11,3 +20,14 @@ class BaseJob(ABC, ValidationMixin):
     @abstractmethod
     def _check_vars(self):
         pass
+
+
+    def get_app(self):
+        c_app = current_app
+        if not c_app:
+            from app import app as c_app
+        return c_app
+
+
+    def get_pickle(self):
+        return pickle
