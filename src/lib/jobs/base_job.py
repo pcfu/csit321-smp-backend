502f291e042d97f69bf6b37ceb05fb0a7c91d521
Peichong <fupeichong@gmail.com>
2021-08-29 22:37:57 +0800

    refactor BaseJob

--- src/lib/jobs/base_job.py
+++ src/lib/jobs/base_job.py
@@ -1,15 +1,25 @@
 import pickle
 from flask import current_app
 from abc import ABC, abstractmethod
+from rq import get_current_job
 from src.lib.clients import FrontendClient
 from .validation_mixin import ValidationMixin
 
 
 class BaseJob(ABC, ValidationMixin):
-    @abstractmethod
-    def __init__(self):
-        super().__init__()
-        self.frontend = FrontendClient
+    @property
+    def frontend(self):
+        return FrontendClient
+
+
+    @property
+    def app(self):
+        return self._get_app()
+
+
+    @property
+    def pickle(self):
+        return pickle
 
 
     @abstractmethod
@@ -22,12 +32,19 @@ class BaseJob(ABC, ValidationMixin):
         pass
 
 
-    def get_app(self):
+    def _get_app(self):
         c_app = current_app
         if not c_app:
             from app import app as c_app
         return c_app
 
 
-    def get_pickle(self):
-        return pickle
+    def _save_job_status(self, status, message=None):
+        job = get_current_job()
+        job.meta = {}
+        job.meta['status'] = status
+        if message:
+            job.meta['message'] = message
+        else:
+            job.meta.pop('message', None)
+        job.save_meta()
