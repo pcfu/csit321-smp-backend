eefead340f4f58b31c2dee1999275f7ba9b3e324
Peichong <fupeichong@gmail.com>
2021-08-29 22:44:25 +0800

    ModelTrainingJob - implement pipeline to training complete

--- src/lib/jobs/model_training_job.py
+++ src/lib/jobs/model_training_job.py
@@ -1,10 +1,11 @@
+import time
 from .base_job import BaseJob
-from rq import get_current_job
 
 
 class ModelTrainingJob(BaseJob):
-    def __init__(self, config_id, stock_id, date_s, date_e):
+    def __init__(self, training_id, config_id, stock_id, date_s, date_e):
         super().__init__()
+        self.training_id = training_id
         self.config_id = config_id
         self.stock_id = stock_id
         self.date_s = date_s
@@ -14,22 +15,25 @@ class ModelTrainingJob(BaseJob):
 
 
     def run(self, *args, **kwargs):
-        job = get_current_job()
-
         try:
-            prices = self.get_prices()
-            self.save_job_status(job, 'prices retrieved')
+            prices = self._get_prices()
+            self._notify_training_started()
 
-            # Update ModelTraining in db to 'training'
-            self.save_job_status(job, 'training started')
+            ### PLACEHOLDER CODE ###
+            print(f'Simulating training for model_training #{self.training_id}')
+            time.sleep(10)
 
-            print(f'Performing job - #{job.get_id()}')
-            self.save_job_status(job, 'training completed')
-            # Update ModelTraining in db to 'done'
+            ### Serialize (De-serialize in prediction job) trained model
+            # key = self.training_id
+            # serialized = self.pickle.dumps( <MODEL>)
+            # self.app.redis.set(key, serialized)
+            # deserialized = self.pickle.loads( self.app.redis.get(key) )
+            ### END PLACEHOLDER CODE ###
+
+            self._notify_training_completed(12.345)   # with placeholder rmse
 
         except Exception as err:
-            self.save_job_status(job, 'error', str(err))
-            # Update ModelTraining in db to 'error'
+            self._notify_error_occurred(str(err))
 
 
     def _check_vars(self):
@@ -40,24 +44,28 @@ class ModelTrainingJob(BaseJob):
             self._raise_date_error('date_e', self.date_e)
 
 
-    def get_prices(self):
+    def _get_prices(self):
+        self._save_job_status('retrieving stock prices')
         res = self.frontend.get_price_histories(self.stock_id, self.date_s, self.date_e)
         if res.get('status') == 'error':
-            raise RunetimeError(res.get('raw_response').reason)
+            raise RuntimeError(res.get('reason'))
         return res.get('data').get('price_histories')
 
 
-    def save_job_status(self, job, status, message=None):
-        job.meta = {}
-        job.meta['status'] = status
-        if message:
-            job.meta['message'] = message
-        else:
-            job.meta.pop('message', None)
-        job.save_meta()
+    def _notify_training_started(self):
+        self._save_job_status('training started')
+        res = self.frontend.update_model_training(self.training_id, 'training')
+        if res.get('status') == 'error':
+            raise RuntimeError(res.get('reason'))
 
 
-### Object Serialization Example
-# serialized = pickle.dumps(job)
-# current_app.redis.set('pickled_obj', serialized)
-# deserialized = pickle.loads(current_app.redis.get('pickled_obj'))
+    def _notify_training_completed(self, rmse):
+        self._save_job_status('training completed', message=f'rmse = {rmse}')
+        res = self.frontend.update_model_training(self.training_id, 'done', rmse=rmse)
+        if res.get('status') == 'error':
+            raise RuntimeError(res.get('reason'))
+
+
+    def _notify_error_occurred(self, e_msg):
+        self._save_job_status('error', message=e_msg)
+        self.frontend.update_model_training(self.training_id, 'error', error_message=e_msg)
