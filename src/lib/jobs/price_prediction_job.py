d7c0d7b11d525e44348da96b8e6d1f4fc84a4de1
Peichong <fupeichong@gmail.com>
2021-09-12 00:10:23 +0800

    PricePredictionJob - refactor to use new PricePredictionLSTM

--- src/lib/jobs/price_prediction_job.py
+++ src/lib/jobs/price_prediction_job.py
@@ -1,4 +1,5 @@
-import time
+import pickle
+from datetime import datetime
 from .base_job import BaseJob
 
 
@@ -15,35 +16,18 @@ class PricePredictionJob(BaseJob):
 
     def run(self, *args, **kwargs):
         try:
-            prices = self._get_prices()
-
-            ### PLACEHOLDER CODE ###
-            print(f'Simulating price prediction for Stock {self.stock_id} ' +
-                  f'using ModelTraining {self.training_id}')
-            time.sleep(5)
-
-            ### De-serialize trained model
             serialized = self.app.redis.get(self.training_id)
-            model = self.pickle.loads(serialized)
-            prediction = model.get_prediction()
-            prediction.update({ 'stock_id': self.stock_id })
-            print("========= PREDICTED_PRICES ==========")
-            print(prediction)
-            print("======================================")
-            ### END PLACEHOLDER CODE ###
+            inducer = pickle.loads(serialized)
+            model = inducer.load_model()
 
-            self._send_prediction_to_frontend(prediction)
+            raw_data = inducer.get_data(self.stock_id, self.date_s, self.date_e)
+            structured_data = inducer.build_prediction_data(raw_data)
+            prices = inducer.get_prediction(model, structured_data)
+            prediction_params = self._build_prediction_params(prices)
+            self._send_prediction_to_frontend(prediction_params)
 
         except Exception as err:
             self._notify_error_occurred(str(err))
-
-
-    def _get_prices(self):
-        self._save_job_status('retrieving stock prices')
-        res = self.frontend.get_price_histories(self.stock_id, self.date_s, self.date_e)
-        if res.get('status') == 'error':
-            raise RuntimeError(res.get('reason'))
-        return res.get('price_histories')
 
 
     def _check_vars(self):
@@ -53,6 +37,24 @@ class PricePredictionJob(BaseJob):
         if not self._validate_date_format(self.date_e):
             self._raise_date_error('date_e', self.date_e)
 
+
+    def _build_prediction_params(self, prices):
+        return {
+            'stock_id': self.stock_id,
+            'entry_date': datetime.strftime(datetime.now(), "%Y-%m-%d"),
+            'nd_max_price': prices[0],
+            'nd_exp_price': prices[0],
+            'nd_min_price': prices[0],
+            'st_max_price': prices[13],
+            'st_exp_price': prices[13],
+            'st_min_price': prices[13],
+            'mt_max_price': prices[13],
+            'mt_exp_price': prices[13],
+            'mt_min_price': prices[13],
+            'lt_max_price': prices[13],
+            'lt_exp_price': prices[13],
+            'lt_min_price': prices[13]
+        }
 
     def _send_prediction_to_frontend(self, prediction):
         self._save_job_status('prediction completed')
