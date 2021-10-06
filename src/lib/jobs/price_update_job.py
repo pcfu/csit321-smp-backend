ae804e5d3c5bf658fa70d3f74144fbe482a1a7e6
Peichong <fupeichong@gmail.com>
2021-10-07 00:30:51 +0800

    add PriceUpdateJob

---
+++ src/lib/jobs/price_update_job.py
@@ -0,0 +1,26 @@
+from .base_job import BaseJob
+from src.lib.clients import Iex
+
+
+class PriceUpdateJob(BaseJob):
+    def __init__(self, symbols, days):
+        super().__init__()
+        self.symbols = symbols
+        self.days = days
+
+
+    def run(self):
+        try:
+            cl = Iex('actual')
+            res = cl.get_batch_prices(self.symbols, self.days)
+            data = res.get('data')
+            params = [{'symbol': k, 'prices': v[::-1]} for k, v in data.items()]
+            self.frontend.insert_price_histories(params)
+
+        except Exception as err:
+            print(err)
+
+
+    def _check_vars(self):
+        # nothing to check
+        pass
