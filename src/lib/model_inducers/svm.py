4d6e96c7dabe05adccebf0ce303c5b7ead45dddd
Peichong <fupeichong@gmail.com>
2021-10-28 18:01:14 +0900

    update SVM model inducer to fit ModelTrainingJob

--- src/lib/model_inducers/svm.py
+++ src/lib/model_inducers/svm.py
@@ -14,9 +14,8 @@ from sklearn.preprocessing import MinMaxScaler
 """
 
 class SVM(BaseInducer):
-        
     def __init__(self, training_id):
-        
+
         """
             Parameters
             ----------
@@ -26,8 +25,10 @@ class SVM(BaseInducer):
 
         model_save_path = f'trained_models/{training_id}'
         super().__init__(model_save_path)
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
 
-    def get_data(self, stock_id, date_start, date_end, model_data_fields):
+
+    def get_data(self, stock_id, date_start, date_end):
         """
             Returns a list of dicts, each containing a price history entry
 
@@ -35,12 +36,6 @@ class SVM(BaseInducer):
             ----------
             stock_id: int | str
                 Stock id of price histories' parent Stock in Frontend
-                
-            stock_id1: int | str
-                Second stock from the same industry as the main stock, to get only "Close" price
-                
-            stock_id2: int | str
-                Second stock from the same industry as the main stock, to get only "Close" price
 
             date_start: str
                 format: 'YYYY-MM-DD'
@@ -51,8 +46,8 @@ class SVM(BaseInducer):
         # Set stock_id1 and stock_id2 column headers for later
         CloseID1 = 'close_1'
         CloseID2 = 'close_2'
-        
-        params = [ stock_id, date_start, date_end, model_data_fields ]
+
+        params = [ stock_id, date_start, date_end, ['date', 'close'] ]
         params_ti = [ stock_id, date_start, date_end ]
         params_sis = [ stock_id, date_start, date_end ]
 
@@ -78,7 +73,7 @@ class SVM(BaseInducer):
         df1 = df1.set_index('date')
         del df1['index']
         del df1['symbol']
-  
+
         df2 = df_list[1]
         df2['date'] = pd.to_datetime(df2['date'])
         df2.rename(columns={'close':CloseID2}, inplace=True)
@@ -90,7 +85,7 @@ class SVM(BaseInducer):
         # Merge the 3 dataframes
         df_merge = pd.merge(df,df1, how='inner', left_index=True, right_index=True)
         df_merge1 = pd.merge(df_merge,df2, how='inner', left_index=True, right_index=True)
-        
+
         # Obtain TI and merge
         df_ti = pd.json_normalize(res_ti['technical_indicators'])
         df_ti['date'] = pd.to_datetime(df_ti['date'])
@@ -102,33 +97,34 @@ class SVM(BaseInducer):
 
         # Final merge for 3 closing price DF with TI df
         df_final = pd.merge(df_merge1,df_ti, how='inner', left_index=True, right_index=True)
-        
+
         return df_final
+
 
     def preprocess_data(self, get_df):
         """
             Setup variables to be use in helper functions
-            
+
             Variables
             ----------
-            targetPriceForward: int 
+            targetPriceForward: int
                 Predict forward n days
-            
+
             targetCol: float
-                Create new column - y target column 
-                
+                Create new column - y target column
+
             refPriceCol / refPriceCol1 / refPriceCol2: float
                 Column to reference - Close price
-                
+
             tar_Days_close_1 / tar_Days_close_2: float
                 Create new y target column for stock 2 and 3
-                
+
             marginCol / marginCol1 / marginCol2: float
-                Create margin column 
-                
+                Create margin column
+
             opinion1 / opinion2: int
                 Create opinion column to store stock 2 and 3 "recommendation"
-                
+
             recommendationCol: int
                 Create recommendation column for main stock
 
@@ -140,28 +136,29 @@ class SVM(BaseInducer):
         buyThreshold = 1
         sellThreshold = -1
         targetPriceForward = 10
-        
+
         targetCol = 'targetCol'
         refPriceCol = 'close'
         marginCol = 'margin_percent'
-        recommendationCol = 'recommendation'        
-        
+        recommendationCol = 'recommendation'
+
         #Setup new column for x days predicted values
         self.newPriceColumn(get_df,-targetPriceForward,refPriceCol,targetCol)
         get_df.dropna(inplace=True)
-        
+
         #Getting margain difference Buy Sell Recommendatiaon
         self.getMarginChange(get_df,targetCol,refPriceCol,marginCol)
-        
+
         #Getting ND Margain Buy Sell Recommendatiaon
         self.recommendation(get_df,marginCol,recommendationCol, sellThreshold, buyThreshold)
 
         return get_df
 
+
     def build_train_test_data(self, data, trainSize):
         """
-            Return dataframe of train and test set. 
-            
+            Return dataframe of train and test set.
+
             Variables
             ---------
             X = Original dataset, without the target column
@@ -172,38 +169,38 @@ class SVM(BaseInducer):
             -------
             x_train, y_train, x_test and y_test
         """
-        
+        data = self.preprocess_data(data)
+
         # Declare features and variables
-        X_features=['sma_5','sma_8','sma_10','wma_5','wma_8',
-                    'wma_10','stoch_k','stoch_d','williams','macd',
-                    'rsi','ad','roc','cci','atr','close_1','close_2']
+        X_features= ['sma_5','sma_8','sma_10','wma_5','wma_8',
+                     'wma_10','stoch_k','stoch_d','williams','macd',
+                     'rsi','ad','roc','cci','atr','close_1','close_2']
         Y_feature = 'recommendation'
-        
+
         # Filter dataframe and get only required features
         X = data[X_features]
         Y = data[Y_feature]
-       
+
         #Split to Train and Test Set
         x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size = trainSize, shuffle=True)
 
         # Normalization train set
-        self.scaler = MinMaxScaler(feature_range=(0, 1)) 
-        arr_scaled = self.scaler.fit_transform(x_train) 
+        arr_scaled = self.scaler.fit_transform(x_train)
         x_train = pd.DataFrame(arr_scaled, columns=x_train.columns,index=x_train.index)
-        
+
         # Normalization test set
         arr_scaled_test = self.scaler.transform(x_test)
         x_test = pd.DataFrame(arr_scaled_test, columns=x_test.columns,index=x_test.index)
 
         return [x_train, x_test, y_train, y_test]
 
+
     def build_model(self, *args):
         """
             BUild the model of SVM by initializing it
         """
-        svm_model = svm.SVC()
+        return svm.SVC()
 
-        return svm_model
 
     def train_model(self, svm_model, model_params, data):
         """
@@ -212,14 +209,14 @@ class SVM(BaseInducer):
 
             Returns
             -------
-            gs_svm: NA
-                Trained and fitted model 
+            model: scikit-leran model object
+                Trained and fitted model
 
-            best_params: dict
-                Best parameters, to be display in frontend
-            
-            f1_score_svm: float
-                performance metric on F1 micro average aka accuracy 
+            accuracy: float
+                performance metric on F1 micro average aka accuracy
+
+            parameters: dict
+                Best model parameters
         """
         # Train, test datasets
         [x_train, x_test, y_train, y_test] = data
@@ -229,27 +226,23 @@ class SVM(BaseInducer):
 
         # Declare time series split for crossValidation
         btss = BlockingTimeSeriesSplit(n_splits=4)
-        
+
         # Cross validation randomized gridsearch
-        gs_svm = RandomizedSearchCV(estimator=svm_model, 
+        gs_svm = RandomizedSearchCV(estimator=svm_model,
                                     cv=btss,
                                     param_distributions=params,
                                     scoring='accuracy')
-        #Fit model
-        gs_svm.fit(x_train, y_train)
 
-        # Best hyperparameters
-        best_params = gs_svm.best_params_
-        
-        # Get performance metric
+        # Train and predict on test set
+        gs_svm.fit(x_train, y_train)
         y_pred_svm = gs_svm.predict(x_test)
-        f1_score_model = f1_score(y_test, y_pred_svm, average='micro')
-        
+
         return {
-            'model_fit': gs_svm,
-            'f1_score': f1_score_model,
-            'best_params': best_params
+            'model': gs_svm,
+            'accuracy': f1_score(y_test, y_pred_svm, average='micro'),
+            'parameters': gs_svm.best_params_
         }
+
 
     def build_prediction_data(self, get_df):
         """
@@ -258,17 +251,18 @@ class SVM(BaseInducer):
             Returns
             -------
             A row of latest data
-        """   
+        """
         # Get last entry
         X_features=['sma_5','sma_8','sma_10','wma_5','wma_8',
                     'wma_10','stoch_k','stoch_d','williams','macd',
                     'rsi','ad','roc','cci','atr','close_1','close_2']
-        
+
         to_predict = get_df[X_features]
-        arr_scaled_predicted = self.scaler.transform(to_predict) 
+        arr_scaled_predicted = self.scaler.transform(to_predict)
         to_predict = pd.DataFrame(arr_scaled_predicted, columns=to_predict.columns,index=to_predict.index)
 
         return to_predict
+
 
     def get_prediction(self, model_fit, data):
         """
@@ -288,19 +282,20 @@ class SVM(BaseInducer):
         else:
             return 'Neutral'
 
+
     def _build_features_and_labels(self, data):
         None
 
     # Helper functions
     """
         Helper functions to be use in preprocessing of data.
-        
+
         newPriceColumn = Creation of new Y target column for n forward (applying shift)
         getMarginChange = Get the difference between current close price against n forward close price in newPriceColumn
         recommendation = Create new column to determine whether the stock should buy, sell or hold
         BlockingTimeSeriesSplit = To split train set into n number of train/test set for model training
     """
-    
+
     #Defining Function to Create New Columns of Prices for X Days
     def newPriceColumn(self, df, Xdays, getColName, setNewColName):
         shift_df = df.shift(periods=Xdays)
@@ -315,7 +310,7 @@ class SVM(BaseInducer):
             df[colInsertName].iloc[counter] = marginChange
             counter=counter+1
 
-    #Defining Function: Populate the recommendation of -1, 0, 1 to indicate sell, neutral or buy. 
+    #Defining Function: Populate the recommendation of -1, 0, 1 to indicate sell, neutral or buy.
     def recommendation(self, df, marginColName, colInsertName, sellThreshold, buyThreshold): #df,10DaysCloseMargin,recommendationCol, sellThreshold, buyThreshold
         counter = 0
         df[colInsertName] = 0
@@ -328,6 +323,7 @@ class SVM(BaseInducer):
                 df[colInsertName].iloc[counter]=0
             counter=counter+1
 
+
 # Helper class
 """
     BlockingTimeSeriesSplit further split the train set into n number of train/test to train the model
@@ -336,10 +332,10 @@ class SVM(BaseInducer):
 class BlockingTimeSeriesSplit():
     def __init__(self, n_splits):
         self.n_splits = n_splits
-    
+
     def get_n_splits(self, X, y, groups):
         return self.n_splits
-    
+
     def split(self, X, y=None, groups=None):
         n_samples = len(X)
         k_fold_size = n_samples // self.n_splits
